caf5110fcf387ba2a6ae789c5a393596d8a32cb2
TheJovster <joca.251@hotmail.com>
2023-03-13 21:45:29 +0100

    Updated EnemyWeapon.cs
    EnemyWeapon behaves as it was intended.

--- Weapon/EnemyWeapon.cs
+++ Weapon/EnemyWeapon.cs
@@ -1,3 +1,4 @@
+using OperationPolygon.Core;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,12 +7,21 @@ namespace OperationPolygon.AI
 {
     public class EnemyWeapon : MonoBehaviour
     {
+        [SerializeField] private AudioSource muzzleAudioSource;
+        [SerializeField] private AudioClip[] gunShotClips;
+        [SerializeField] private ParticleSystem gunShotParticle;
+
         [SerializeField] private Transform muzzlePoint;
         [SerializeField] private GameObject projectilePrefab;
         [SerializeField] private int damage = 3;
         [SerializeField] private float fireRate = 1f;
         private float timeSinceLastShot = 0f;
-        
+        private Health health;
+
+        private void Awake()
+        {
+            health = GetComponentInParent<Health>();
+        }
 
         private void Update()
         {
@@ -20,12 +30,19 @@ namespace OperationPolygon.AI
 
         public void Shoot()
         {
-            if (timeSinceLastShot >= fireRate)
+            if (health.IsAlive())
             {
-                Instantiate(projectilePrefab, muzzlePoint.position, muzzlePoint.rotation);
-                timeSinceLastShot = 0f;
-                Debug.Log("Shot at player");
+                if (timeSinceLastShot >= fireRate)
+                {
+                    int randomClipIndex = Random.Range(0, gunShotClips.Length);
+                    muzzleAudioSource.PlayOneShot(gunShotClips[randomClipIndex]);
+                    gunShotParticle.Play();
+                    Instantiate(projectilePrefab, muzzlePoint.position, muzzlePoint.rotation);
+                    timeSinceLastShot = 0f;
+                    Debug.Log("Shot at player");
+                }
             }
+
         }
 
         public int GetDamage() 
